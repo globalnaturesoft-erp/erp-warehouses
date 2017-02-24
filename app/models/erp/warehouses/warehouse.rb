a1921adebd80ff95c77a1ed07c3ca1d0953185c5
sonnguyenngoc <sonnn@hoangkhang.com.vn>
2017-02-24 01:54:01 -0800

    review warehouse 24022017

--- app/models/erp/warehouses/warehouse.rb
+++ app/models/erp/warehouses/warehouse.rb
@@ -101,11 +101,11 @@ module Erp::Warehouses
 		end
     
     def self.archive_all
-			update_all(archived: false)
+			update_all(archived: true)
 		end
     
     def self.unarchive_all
-			update_all(archived: true)
+			update_all(archived: false)
 		end
     
     # display name
